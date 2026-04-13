72b9e68b58073e85362881046fe9fa4546943faf
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-04-13 22:35:18 +0800

    refactor: migrate to everforest theme

---
+++ content/config/nvim/plugin/theme.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+    { src = "https://github.com/sainnhe/everforest" },
+})
+
+vim.g.everforest_background = 'hard'
+vim.cmd.colorscheme("everforest")
--- content/config/nvim/plugin/tokyonight.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({
-	{ src = "https://github.com/folke/tokyonight.nvim" },
-})
-
-vim.cmd.colorscheme("tokyonight-night")
