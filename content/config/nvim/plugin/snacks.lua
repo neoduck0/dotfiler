81e3eb88229a1ecff461ac27eddf527ed643ed26
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-05-13 01:23:30 +0800

    refactor(neovim): add dashboard

--- content/config/nvim/plugin/snacks.lua
+++ content/config/nvim/plugin/snacks.lua
@@ -4,6 +4,25 @@ vim.pack.add({
 })
 
 require("snacks").setup({
+    dashboard = {
+        enabled = true,
+        sections = {
+            { section = "header" },
+            { section = "keys",  gap = 1, padding = 1 },
+            -- { section = "recent_files", icon = " ", title = "Recent Files", padding = 1 },
+            -- { section = "startup" },
+        },
+        preset = {
+            keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            }
+        }
+    },
     explorer = { enabled = true },
     picker = {
         enabled = true,
